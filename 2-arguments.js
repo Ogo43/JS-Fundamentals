36c5d0442d1e88a84a694dc1d05981f93c1b489c
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-05-24 03:17:14 -0700

    Determine argument using control flow

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const argument = [0, "Best", "Best School"];
 
-if (argument.length === "") {
+if (argument.length === 0) {
   console.log("No argument");
-} else if (argument.length >= 0 && argument.length <= 16) {
+} else if (argument.length >= 0 && argument.length <= 4) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
