150f149f6b34b2bc4d99656ee22ccef84aaa819d
Ogochukwu Emeh <emehaogochukwu@gmail.com>
2025-05-24 06:50:51 -0700

    Print C is fun multiple times

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+let argTimes = process.argv[2];
+let x = parseInt(argTimes);
+
+if (isNaN(x)) {
+        console.log("Missing number of occurrences");
+    }else{
+        for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
